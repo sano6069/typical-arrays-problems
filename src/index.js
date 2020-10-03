b608f650abbcff3c2b3410505e5a26c2aa3668d1
Alex <sano6069@gmail.com>
2020-10-03 14:17:08 +0300

    Feat: add max,min,avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+
+  if (Array.isArray(array)){
+    let min = 0;
+    let length = array.length;
+    while (length--) {
+      if (array[length] < min) {
+        min = array[length];
+      }
+    }
+    return min;
+  }
+    else return 0;
+  }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (Array.isArray(array)){
+    let max = 0;
+    let length = array.length;
+    while (length--) {
+      if (array[length] > max) {
+        max = array[length];
+      }
+    }
+    return max;
+  }
+  else return 0;
+  }
+
 
 exports.avg = function avg (array) {
-  return 0;
+if( array === undefined || array.length === 0 )
+    return 0;
+    let count = 0, sum =0;
+    let len = array.length;
+    for(let i =0; i < len; i++)
+    sum+= array[i];
+    count = sum/(len);
+    return  count;
 }
